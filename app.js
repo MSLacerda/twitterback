e014df7334995196c05184e3ab0526bb0eb24e7d
matheus soares de lacerda soares <matheus soares de lacerda soares>
2017-07-02 14:25:16 -0300

    Startig script

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var app = require('./app/config/server');
 
-app.listen(3000, function() {
-    console.log('Servidor Online '+process.env.PORT);
+app.listen(3000 || process.env.PORT, function() {
+    console.log('Servidor Online ');
 });
